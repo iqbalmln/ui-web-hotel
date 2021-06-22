d155236ca3db7aa6e23385d35f05666da3ec3e10
Iqbal Maulana <masadepansilauu@gmail.com>
2021-06-22 22:09:36 +0700

    Create UI Web-Hotel using gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,13 @@
-var gulp = require('gulp');
-var gulpConnect = require('gulp-connect');
-var gulpMinifyCss = require('gulp-minify-css');
-var gulpConcat = require('gulp-concat');
-var gulpUglify = require('gulp-uglify');
-var gulpHtmlmin = require('gulp-htmlmin');
-var clean = require('gulp-clean');
+const gulp = require('gulp');
+const gulpConnect = require('gulp-connect');
+const gulpMinifyCss = require('gulp-minify-css');
+const gulpConcat = require('gulp-concat');
+const gulpUglify = require('gulp-uglify');
+const gulpHtmlmin = require('gulp-htmlmin');
+const clean = require('gulp-clean');
+const image = require('gulp-image');
+const fileinclude = require('gulp-file-include');
 
-gulp.task('sayHello', async function () {
-    console.log("Hello, selamat datang di Gulp!");
-});
 
 gulp.task('server', async function () {
     gulpConnect.server({
@@ -45,13 +44,25 @@ gulp.task('minify-html', async function () {
         .pipe(gulp.dest('dist'))
         .pipe(gulpConnect.reload());
 });
+gulp.task('image', async function () {
+    gulp.src('./src/images/*')
+    .pipe(image())
+    .pipe(gulp.dest('dist/image'))
+})
 
 gulp.task('watch', async function () {
     gulp.watch('./src/js/*.js', gulp.series('minify-js'));
     gulp.watch('./src/css/*.css', gulp.series('minify-css'));
     gulp.watch('./src/*.html', gulp.series('minify-html'));
 });
-
+gulp.task('files', function() {
+    gulp.src(['src/index.html'])
+    .pipe(fileinclude({
+        prefix: '@@',
+        basepath: '@file'
+    }))
+    .pipe(gulp.dest('./dist'))
+})
 
 
 gulp.task('clean', function() {
@@ -61,6 +72,6 @@ gulp.task('clean', function() {
   }).pipe(clean());
 });
 
-gulp.task('build', gulp.series('clean', 'minify-css', 'minify-js', 'minify-html'));
+gulp.task('build', gulp.series('clean', 'minify-css', 'minify-js', 'minify-html', 'image', 'files'));
 
 gulp.task('default', gulp.series('watch', 'server'));
